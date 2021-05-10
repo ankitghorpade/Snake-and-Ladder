17683c7f5164cc45da90ca77f146cf0679eecd3a
unknown <ankitghorpade59@gmail.com>
2021-05-10 09:38:43 -0800

    Welcome

---
+++ snake.java
@@ -0,0 +1,12 @@
+package com;
+
+public class snake {
+		 	public static void main(String[] args) {
+		 		 //variable
+		         int singleplayer = 0;
+		         System.out.println("single player at start position is: " + singleplayer);
+
+		 	}
+
+		 }
+		 
