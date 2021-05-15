22b352672d5ce42b7b639251e781f80a21f1abc9
ankitghorpade <75006687+ankitghorpade@users.noreply.github.com>
2021-05-15 06:09:14 -0800

    Update snake.java

--- snake.java
+++ snake.java
@@ -3,8 +3,8 @@ package com;
 public class snake {
 		 	public static void main(String[] args) {
 		 		 //variable
-		         int singleplayer = 0;
-		         System.out.println("single player at start position is: " + singleplayer);
+		         int n = 0;
+		         System.out.println("single player at start position is: " + n);
 
 		 	}
 
