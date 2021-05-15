8a5fb61bf2e09440d86d053ab8ff810c1d89172b
unknown <ankitghorpade59@gmail.com>
2021-05-15 01:58:10 -0800

    adding uc2

--- snake.java
+++ snake.java
@@ -1,10 +1,17 @@
 package com;
+import java.util.Random;
 
-public class snake {
+
+  public class snake{
 		 	public static void main(String[] args) {
 		 		 //variable
-		         int singleplayer = 0;
-		         System.out.println("single player at start position is: " + singleplayer);
+		         int n = 0;
+		         Random Dice = new Random();
+		       
+		       String[] str = {"one", "Two", "Three" ,"Four","Five","Six"};
+		       int roll = Dice.nextInt(6);
+		       
+		         System.out.println(str[roll]);
 
 		 	}
 
